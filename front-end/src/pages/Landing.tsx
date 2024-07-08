176be4c2bc420ce5f63f5a841d126df433333885
GreatnessBarthNathan <ekemeziebartholomew@gmail.com>
2024-07-08 18:59:56 +0100

    .

--- front-end/src/pages/Landing.tsx
+++ front-end/src/pages/Landing.tsx
@@ -7,11 +7,18 @@ function Landing() {
       <div className='w-[85%] m-auto flex space-x-5 py-5 overflow-auto'>
         {/* left part */}
         <div className='w-full lg:w-[50%]'>
-          <Logo container='w-[100px] md:w-[150px]' />
+          {/* <Logo container='w-[100px] md:w-[150px]' /> */}
+          {/* <img src='/Vector-3.png' /> */}
+          <div className='flex items-center justify-between space-x-5'>
+            <Logo container='w-[100px] md:w-[150px]' />
+            <div className='w-[80%]'>
+              <img src='/Vector-3.png' className='w-full' />
+            </div>
+          </div>
           <div>
-            <h1 className='text-3xl lg:text-5xl font-bold text-[var(--red)] mt-10'>
+            {/* <h1 className='text-3xl lg:text-5xl font-bold text-[var(--red)] mt-10'>
               Fruit <span className='text-[var(--orange)]'>of </span>The Vine
-            </h1>
+            </h1> */}
 
             {/* SMALL SCREEN IMAGE */}
             <div className='md:hidden w-full m-auto mt-3 rounded-md overflow-hidden shadow-lg'>
@@ -22,19 +29,39 @@ function Landing() {
                 className='w-full'
               />
             </div>
-            <p className='mt-5 text-slate-700 leading-7 text-sm md:text-base lg:leading-8'>
+            <p className='mt-5 text-slate-700 leading-7 text-sm md:text-base lg:leading-5'>
+              <span className='block text-[var(--red)] font-bold'>
+                ABOUT US
+              </span>
               A ministry-affiliated mini-mart offering high-quality provisions
               to the community, blending spiritual mission with practical
               service.
               <p className='mt-5'>
+                <span className='block text-[var(--red)] font-bold'>
+                  VISION
+                </span>
                 Our Vision is to be a community beacon, delivering quality and
                 service that nourishes both body and spirit, reflecting our
                 ministry's values
               </p>
               <p className='mt-5'>
+                <span className='block text-[var(--red)] font-bold'>
+                  MISSION
+                </span>
                 Our Mission is to provide esssential goods in a welcoming,
                 spiritually enriching environment, serving our community with
                 integrity and compassion.
+              </p>
+              <p className='mt-5'>
+                <span className='block text-[var(--red)] font-bold'>
+                  CORE VALUES
+                </span>
+                <div className=''>
+                  <li>Welcoming</li>
+                  <li>Friendly</li>
+                  <li>Vibrant</li>
+                  <li>Dependable</li>
+                </div>
               </p>
             </p>
             <div className='mt-10'>
@@ -59,7 +86,6 @@ function Landing() {
             {/* <img src='/landing_img.svg' alt='' /> */}
             <img
               src='https://i.pinimg.com/564x/7b/d4/66/7bd46623d484c73fdbd198e4c85968e5.jpg'
-              // src='/test1.png'
               alt=''
               className='w-full'
             />

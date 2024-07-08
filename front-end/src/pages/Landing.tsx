e3e8367d9a9fa9af7f470e9e9291d2e6eced6c1b
GreatnessBarthNathan <ekemeziebartholomew@gmail.com>
2024-07-08 15:33:18 +0100

    added landing image to small screen

--- front-end/src/pages/Landing.tsx
+++ front-end/src/pages/Landing.tsx
@@ -3,15 +3,24 @@ import Logo from "../components/Logo"
 
 function Landing() {
   return (
-    <div className='w-[85%] h-[100dvh] m-auto flex space-x-5 py-5 overflow-auto'>
+    <div className='w-[85%] m-auto flex space-x-5 py-5 overflow-auto'>
       {/* left part */}
       <div className='w-full lg:w-[50%]'>
         <Logo container='w-[100px] md:w-[150px]' />
         <div>
-          <h1 className='text-2xl lg:text-5xl font-bold text-[#05156D] mt-10'>
+          <h1 className='text-3xl lg:text-5xl font-bold text-[#05156D] mt-10'>
             Fruit <span className='text-cyan-500 capitalize'>Of The</span> Vine
           </h1>
 
+          {/* SMALL SCREEN IMAGE */}
+          <div className='md:hidden w-full m-auto mt-3 rounded-md overflow-hidden shadow-lg'>
+            {/* <img src='/landing_img.svg' alt='' /> */}
+            <img
+              src='https://i.pinimg.com/564x/7b/d4/66/7bd46623d484c73fdbd198e4c85968e5.jpg'
+              alt=''
+              className='w-full'
+            />
+          </div>
           <p className='mt-5 text-slate-700 leading-7 text-sm md:text-base lg:leading-8'>
             A ministry-affiliated mini-mart offering high-quality provisions to
             the community, blending spiritual mission with practical service.
